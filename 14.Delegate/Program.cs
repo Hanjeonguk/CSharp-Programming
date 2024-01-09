806f55df009f7e3280265fb6e5f72954d94813be
Hanjeonguk <wjddnr1358@naver.com>
2024-01-09 18:32:01 +0900

    docs : 델리게이트
    대리자 : 특정 매개 변수 목록 및 반환 형식이 있는 함수에 대한 참조를 나타내는 형식

---
+++ 14.Delegate/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _14.Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
